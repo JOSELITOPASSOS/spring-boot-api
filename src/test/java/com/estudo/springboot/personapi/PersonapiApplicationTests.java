fac6489fcdb2a023eac49c3f3edee49fc5417b0c
unknown <joselitomartinho@gmail.com>
2021-03-08 22:18:57 -0300

    Projeto inicial gerado Spring Start.

---
+++ src/test/java/com/estudo/springboot/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.springboot.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
