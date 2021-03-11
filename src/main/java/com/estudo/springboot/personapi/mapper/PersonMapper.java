b381fe1b5c35af8984c932c5f22510a83b11db65
unknown <joselitomartinho@gmail.com>
2021-03-11 00:19:57 -0300

    Importado projeto + Correção problemas.

---
+++ src/main/java/com/estudo/springboot/personapi/mapper/PersonMapper.java
@@ -0,0 +1,18 @@
+package com.estudo.springboot.personapi.mapper;
+
+import com.estudo.springboot.personapi.dto.request.PersonDTO;
+import com.estudo.springboot.personapi.entity.Person;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    Person toModel(PersonDTO personDTO);
+
+    PersonDTO toDTO(Person person);
+}
